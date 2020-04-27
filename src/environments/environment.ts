ff8961a8ab7d7fb18118ddd4c84fa3e5abca3338
Yusuke Gunji <inochideutae@gmail.com>
2020-04-27 13:34:04 +0900

    feat:AangularFire導入#4

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  apiKey: 'AIzaSyDp1KDhs5NvmdsItuBoGV9YRtN3tDzlb6E',
+  authDomain: 'school-potal-aaf0b.firebaseapp.com',
+  databaseURL: 'https://school-potal-aaf0b.firebaseio.com',
+  projectId: 'school-potal-aaf0b',
+  storageBucket: 'school-potal-aaf0b.appspot.com',
+  messagingSenderId: '286244257920',
+  appId: '1:286244257920:web:7cdf70fc84176cbf5f2665',
+  measurementId: 'G-0H2YHQ47YQ'
+  }
 };
 
 /*
