e8c00ddfb7b789409f8659882c79fadee2417fc8
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-05-01 06:25:59 +0900

    fix(sidenav): 高さ調整、動作最適化

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,11 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'school-potal';
-  opened: boolean;
+  opened$: Observable<boolean> = this.drawerservice.isOpen$;
 
-  constructor(
-    private drawerservice: DrawerService
-  ) {
-    this.drawerservice.isOpen$.subscribe(opened => this.opened = opened);
-  }
+  constructor(private drawerservice: DrawerService) {}
 }
