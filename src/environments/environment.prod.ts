e7ef23440cdea31af809bd2b133cf069bd2ab7f1
Yusuke Gunji <inochideutae@gmail.com>
2020-05-13 19:49:38 +0900

    feat(github.workflows):github Actions導入

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDp1KDhs5NvmdsItuBoGV9YRtN3tDzlb6E',
+    authDomain: 'school-potal-aaf0b.firebaseapp.com',
+    databaseURL: 'https://school-potal-aaf0b.firebaseio.com',
+    projectId: 'school-potal-aaf0b',
+    storageBucket: 'school-potal-aaf0b.appspot.com',
+    messagingSenderId: '286244257920',
+    appId: '1:286244257920:web:7cdf70fc84176cbf5f2665',
+    measurementId: 'G-0H2YHQ47YQ',
+  },
 };
