026b7d1ab8bbad25df1c20d348f1b2bdf6890585
Yusuke Gunji <inochideutae@gmail.com>
2020-05-13 14:01:23 +0900

    feat(editor.component):エディター画面追加

---
+++ src/app/editor/editor.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EditorRoutingModule } from './editor-routing.module';
+import { EditorComponent } from './editor/editor.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  declarations: [EditorComponent],
+  imports: [
+    CommonModule,
+    EditorRoutingModule,
+    MatDividerModule,
+    MatSelectModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatTabsModule,
+    MatTableModule,
+  ],
+})
+export class EditorModule {}
