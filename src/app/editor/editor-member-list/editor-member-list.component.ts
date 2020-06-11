722dd99e30b2956c3fc42cd49f472769bc5a64ae
Yusuke Gunji <inochideutae@gmail.com>
2020-06-12 04:57:26 +0900

    feat(editor-member-list): メンバー管理画面追加

---
+++ src/app/editor/editor-member-list/editor-member-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-member-list',
+  templateUrl: './editor-member-list.component.html',
+  styleUrls: ['./editor-member-list.component.scss'],
+})
+export class EditorMemberListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
