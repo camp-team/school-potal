e6632c4a7f76dab67d72255d7c393723f9de5c12
Yusuke Gunji <inochideutae@gmail.com>
2020-06-29 03:36:33 +0900

    feat(user.function): region追加

--- functions/src/create-user.function.ts
+++ functions/src/create-user.function.ts
@@ -3,11 +3,14 @@ import * as admin from 'firebase-admin';
 
 export const db = admin.firestore();
 
-export const createUser = functions.auth.user().onCreate((user) => {
-  return db.doc(`users/${user.uid}`).set({
-    name: user.displayName,
-    photoURL: user.photoURL,
-    email: user.email,
-    createdAt: new Date(),
+export const createUser = functions
+  .region('asia-northeast1')
+  .auth.user()
+  .onCreate((user) => {
+    return db.doc(`users/${user.uid}`).set({
+      name: user.displayName,
+      photoURL: user.photoURL,
+      email: user.email,
+      createdAt: new Date(),
+    });
   });
-});
