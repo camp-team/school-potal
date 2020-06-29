d28288a998a9613be66759d71c7d62d5516f06c0
Yusuke Gunji <inochideutae@gmail.com>
2020-06-30 02:54:23 +0900

    feat(user.function): createUserWithTwitter追加

--- functions/src/create-user.function.ts
+++ functions/src/create-user.function.ts
@@ -3,7 +3,19 @@ import * as admin from 'firebase-admin';
 
 export const db = admin.firestore();
 
-export const createUser = functions
+export const createUserWithGoogle = functions
+  .region('asia-northeast1')
+  .auth.user()
+  .onCreate((user) => {
+    return db.doc(`users/${user.uid}`).set({
+      name: user.displayName,
+      photoURL: user.photoURL,
+      email: user.email,
+      createdAt: new Date(),
+    });
+  });
+
+export const createUserWithTwitter = functions
   .region('asia-northeast1')
   .auth.user()
   .onCreate((user) => {
