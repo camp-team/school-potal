0dba0960954a43943c89dd24ac9eef13d452d21e
Yusuke Gunji <inochideutae@gmail.com>
2020-07-15 05:22:28 +0900

    fix(twitter.fns): setTeacherData関数の修正

--- functions/src/twitter.function.ts
+++ functions/src/twitter.function.ts
@@ -6,8 +6,8 @@ export const db = admin.firestore();
 
 export const setTeacherDataById = functions
   .region('asia-northeast1')
-  .https.onCall(async (teacherId) => {
-    const masterId: string = functions.config().masterid;
+  .https.onCall(async (param) => {
+    const masterId: string = functions.config().twitter.masterid;
     const twitterData = (
       await db.doc(`users/${masterId}/private/twitter`).get()
     ).data();
@@ -16,20 +16,22 @@ export const setTeacherDataById = functions
 
     if (twitterData) {
       const twitterClient = new Twitter({
-        consumer_key: functions.config().twitter.consumer_key,
-        consumer_secret: functions.config().twitter.consumer_secret,
+        consumer_key: functions.config().credential.twitter.consumer_key,
+        consumer_secret: functions.config().credential.twitter.consumer_secret,
         access_token_key: functions.config().twitter.token_key,
-        access_token_secret: functions.config().twitter.toke_secret,
+        access_token_secret: functions.config().twitter.token_secret,
       });
       const TwitterProfile = await twitterClient.get('users/show', {
-        screen_name: teacherId,
+        screen_name: param.teacherId,
       });
 
-      await db.doc(`articles/${teacherId}/teachers/teacherData`).set({
-        screen_name: TwitterProfile.screen_name,
-        description: TwitterProfile.description,
-        profile_banner_url: TwitterProfile.profile_banner_url,
-      });
+      await db
+        .doc(`articles/${param.articleId}/teachers/${param.teacherId}`)
+        .set({
+          screen_name: TwitterProfile.screen_name,
+          description: TwitterProfile.description,
+          profile_banner_url: TwitterProfile.profile_banner_url,
+        });
       console.log(TwitterProfile);
 
       return true;
