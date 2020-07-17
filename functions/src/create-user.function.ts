23c52e7587372cd6e1c20e1c372b3270115b9dde
Yusuke Gunji <inochideutae@gmail.com>
2020-07-17 11:53:08 +0900

    feat(functions): createUser削除createUserTokyo追加

--- functions/src/create-user.function.ts
+++ functions/src/create-user.function.ts
@@ -3,7 +3,7 @@ import * as admin from 'firebase-admin';
 
 export const db = admin.firestore();
 
-export const createUser = functions
+export const createUserTokyo = functions
   .region('asia-northeast1')
   .auth.user()
   .onCreate((user) => {
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,6 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp(functions.config().firebase);
 
-export { createUser } from './create-user.function';
+export { createUserTokyo } from './create-user.function';
 
 export { setTeacherDataById } from './twitter.function';
