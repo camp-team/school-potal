2362ee0b53e87539dde572656d88dc032dc08c3a
Yusuke Gunji <inochideutae@gmail.com>
2020-07-21 11:25:20 +0900

    feat(functions): algolia関数追加

---
+++ functions/src/article.function.ts
@@ -0,0 +1,42 @@
+import { Algolia } from './utils/algolia.function';
+import * as functions from 'firebase-functions';
+
+const algolia = new Algolia();
+
+export const createArticle = functions
+  .region('asia-northeast1')
+  .firestore.document('articles/{id}')
+  .onCreate((snap) => {
+    const data = snap.data();
+    return algolia.saveRecord({
+      indexName: 'articles',
+      largeConcentKey: 'body',
+      data,
+    });
+  });
+
+export const deleteArticle = functions
+  .region('asia-northeast1')
+  .firestore.document('articles/{id}')
+  .onDelete((snap) => {
+    const data = snap.data();
+
+    if (data) {
+      return algolia.removeRecord('articles', data.id);
+    } else {
+      return;
+    }
+  });
+
+export const updateArticle = functions
+  .region('asia-northeast1')
+  .firestore.document('articles/{id}')
+  .onUpdate((change) => {
+    const data = change.after.data();
+    return algolia.saveRecord({
+      indexName: 'articles',
+      largeConcentKey: 'body',
+      isUpdate: true,
+      data,
+    });
+  });
