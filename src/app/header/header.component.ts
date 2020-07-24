a50ab9367988bc8fded8f3882c97a4d9a745fcc1
Yusuke Gunji <inochideutae@gmail.com>
2020-07-24 18:08:02 +0900

    feat(header): routSearchとautocomplete追加

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,6 +3,11 @@ import { DrawerService } from '../services/drawer.service';
 import { AuthService } from '../services/auth.service';
 import { Observable } from 'rxjs';
 import { User } from '../interfaces/users';
+import { FormControl } from '@angular/forms';
+import { SearchService } from '../services/search.service';
+import { SearchIndex } from 'algoliasearch/lite';
+import { Router } from '@angular/router';
+import { startWith, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -11,11 +16,30 @@ import { User } from '../interfaces/users';
 })
 export class HeaderComponent implements OnInit {
   user$: Observable<User> = this.authService.user$;
+  searchControl: FormControl = new FormControl();
+  index: SearchIndex = this.searchService.index.item;
+
+  result: {
+    nbHits: number;
+    hits: any[];
+  };
+
+  searchOptions = [];
 
   constructor(
     private drawerService: DrawerService,
-    private authService: AuthService
-  ) {}
+    private authService: AuthService,
+    private searchService: SearchService,
+    private router: Router
+  ) {
+    this.searchControl.valueChanges
+      .pipe(startWith(''), debounceTime(500))
+      .subscribe((key) => {
+        this.index
+          .search(key)
+          .then((result) => (this.searchOptions = result.hits));
+      });
+  }
 
   ngOnInit(): void {}
 
@@ -25,5 +49,18 @@ export class HeaderComponent implements OnInit {
 
   logout() {
     this.authService.logout();
+  }
+
+  setSearchQuery(value) {
+    this.searchControl.setValue(value, {
+      emitEvent: false,
+    });
+  }
+
+  routeSearch(searchQuery: string) {
+    this.router.navigate(['/search'], {
+      queryParamsHandling: 'merge',
+      queryParams: { searchQuery },
+    });
   }
 }
