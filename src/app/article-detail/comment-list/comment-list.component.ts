e10984322f549a9e8d015484a70d3ebf32f7e72f
Yusuke Gunji <inochideutae@gmail.com>
2020-08-09 07:45:40 +0900

    feat(comment-list): コメントとユーザーの実データ表示

--- src/app/article-detail/comment-list/comment-list.component.ts
+++ src/app/article-detail/comment-list/comment-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { CommentService } from 'src/app/services/comment.service';
+import { ArticleService } from 'src/app/services/article.service';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { CommentWithUser, Comment } from 'src/app/interfaces/comment';
 
 @Component({
   selector: 'app-comment-list',
@@ -6,7 +12,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment-list.component.scss'],
 })
 export class CommentListComponent implements OnInit {
-  constructor() {}
+  articleId: string;
+  comments$: Observable<CommentWithUser[]> = this.route.paramMap.pipe(
+    switchMap((map) => {
+      this.articleId = map.get('articleId');
+      return this.commentService.getCommentsWithUserByArticleId(this.articleId);
+    }),
+    tap((data) => console.log(data))
+  );
+
+  constructor(
+    private commentService: CommentService,
+    private articleService: ArticleService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {}
 }
