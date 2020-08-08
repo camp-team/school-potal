dde975e7e263e64fb3da508fa8fd73d778d92742
Yusuke Gunji <inochideutae@gmail.com>
2020-08-09 07:37:56 +0900

    feat(user.service): user$の定義追加

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { User } from '../interfaces/users';
+import { Observable, of } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private db: AngularFirestore) {}
+  uId: string;
+
+  user$: Observable<User> = this.afAuth.authState.pipe(
+    switchMap((user) => {
+      if (user) {
+        this.uId = user.uid;
+        return this.getUserData(this.uId);
+      } else {
+        return of(null);
+      }
+    })
+  );
+
+  constructor(private db: AngularFirestore, private afAuth: AngularFireAuth) {}
 
   getUserData(userId: string) {
     return this.db.doc<User>(`users/${userId}`).valueChanges();
