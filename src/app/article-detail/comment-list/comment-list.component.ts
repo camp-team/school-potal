61301f90a08413e2fa0345292c707d2ce31547e4
Yusuke Gunji <inochideutae@gmail.com>
2020-08-10 16:32:52 +0900

    feat(comment-list): user$追加

--- src/app/article-detail/comment-list/comment-list.component.ts
+++ src/app/article-detail/comment-list/comment-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommentService } from 'src/app/services/comment.service';
-import { ArticleService } from 'src/app/services/article.service';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-import { CommentWithUser, Comment } from 'src/app/interfaces/comment';
+import { CommentWithUser } from 'src/app/interfaces/comment';
+import { User } from 'src/app/interfaces/users';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-comment-list',
@@ -12,6 +13,8 @@ import { CommentWithUser, Comment } from 'src/app/interfaces/comment';
   styleUrls: ['./comment-list.component.scss'],
 })
 export class CommentListComponent implements OnInit {
+  user$: Observable<User> = this.authService.user$;
+
   articleId: string;
   comments$: Observable<CommentWithUser[]> = this.route.paramMap.pipe(
     switchMap((map) => {
@@ -23,7 +26,7 @@ export class CommentListComponent implements OnInit {
 
   constructor(
     private commentService: CommentService,
-    private articleService: ArticleService,
+    private authService: AuthService,
     private route: ActivatedRoute
   ) {}
 
