5556d02f9baa1d532333728fe55b9205380c33b0
Yusuke Gunji <inochideutae@gmail.com>
2020-08-13 23:59:18 +0900

    feat(account): アカウントページマークアップ

--- src/app/user/account/account.component.ts
+++ src/app/user/account/account.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { switchMap, map, tap } from 'rxjs/operators';
+import { UserService } from 'src/app/services/user.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/interfaces/users';
 
 @Component({
   selector: 'app-account',
@@ -6,7 +13,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.scss'],
 })
 export class AccountComponent implements OnInit {
-  constructor() {}
+  uId: string;
+  plan: string;
+
+  userId$: Observable<string> = this.route.paramMap.pipe(
+    map((param) => {
+      this.uId = param.get('uid');
+      return this.uId;
+    })
+  );
+
+  user$: Observable<User> = this.authService.user$;
+
+  constructor(private route: ActivatedRoute, private authService: AuthService) {
+    this.userId$.subscribe((uid) => (this.uId = uid));
+  }
 
   ngOnInit(): void {}
 }
