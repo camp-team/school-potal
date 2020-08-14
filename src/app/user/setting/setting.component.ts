6904cd1ec5434aa9304ba037cadb2dff13013516
Yusuke Gunji <inochideutae@gmail.com>
2020-08-14 16:02:00 +0900

    feat(setting): settingコンポーネントマークアップ

--- src/app/user/setting/setting.component.ts
+++ src/app/user/setting/setting.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from 'src/app/interfaces/users';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-setting',
@@ -6,7 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./setting.component.scss'],
 })
 export class SettingComponent implements OnInit {
-  constructor() {}
+  user$: Observable<User> = this.authService.user$;
 
-  ngOnInit(): void {}
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
+
+  form = this.fb.group({
+    name: ['', Validators.maxLength(50)],
+    profile: ['', Validators.maxLength(400)],
+    link: ['', Validators.maxLength(400)],
+    joins: ['', Validators.maxLength(50)],
+  });
+
+  get name(): FormControl {
+    return this.form.get('name') as FormControl;
+  }
+  get profile(): FormControl {
+    return this.form.get('profile') as FormControl;
+  }
+  get link(): FormControl {
+    return this.form.get('link') as FormControl;
+  }
+  get joins(): FormControl {
+    return this.form.get('joins') as FormControl;
+  }
+
+  ngOnInit(): void {
+    this.user$.subscribe((user) => this.form.patchValue(user));
+  }
+
+  submit() {}
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,9 +4,25 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { AccountComponent } from './account/account.component';
 import { SettingComponent } from './setting/setting.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AccountComponent, SettingComponent],
-  imports: [CommonModule, UserRoutingModule],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    MatFormFieldModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatChipsModule,
+    MatButtonModule,
+  ],
 })
 export class UserModule {}
