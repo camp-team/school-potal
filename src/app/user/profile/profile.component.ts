93af0cecf18793655875f0d01d5a34c57c045390
Yusuke Gunji <inochideutae@gmail.com>
2020-08-16 12:54:41 +0900

    feat(profile):profileコンポーネント追加

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { User } from 'src/app/interfaces/users';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, map, tap } from 'rxjs/operators';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -9,9 +12,19 @@ import { User } from 'src/app/interfaces/users';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  user$: Observable<User> = this.authService.user$;
+  user$: Observable<User> = this.route.paramMap.pipe(
+    switchMap((param) => {
+      const profileId = param.get('uid');
+      return this.userService.getUserData(profileId);
+    }),
+    tap((user) => console.log(user))
+  );
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {}
 }
