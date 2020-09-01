41cda0f0defadeb45bd61824aae8f0ff0c403ec3
Yusuke Gunji <inochideutae@gmail.com>
2020-09-02 02:27:14 +0900

    feat(nav): side-navの引越し

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { DrawerService } from '../services/drawer.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
+  opened$: Observable<boolean> = this.drawerService.isOpen$;
 
-  constructor() { }
+  constructor(private drawerService: DrawerService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
