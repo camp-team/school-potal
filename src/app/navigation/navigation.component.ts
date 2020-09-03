5432cae6b370c08b971bc122ec449e15c7387740
Yusuke Gunji <inochideutae@gmail.com>
2020-09-03 19:56:08 +0900

    feat(nav): search-resultへ遷移へ変更

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DrawerService } from '../services/drawer.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -10,7 +11,14 @@ import { Observable } from 'rxjs';
 export class NavigationComponent implements OnInit {
   opened$: Observable<boolean> = this.drawerService.isOpen$;
 
-  constructor(private drawerService: DrawerService) {}
+  constructor(private drawerService: DrawerService, private router: Router) {}
 
   ngOnInit(): void {}
+
+  routeCategoryFilter(category: string) {
+    this.router.navigate(['/search'], {
+      queryParamsHandling: 'merge',
+      queryParams: { category },
+    });
+  }
 }
