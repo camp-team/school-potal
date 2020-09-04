907f23bc746462c4746a7391ce3c30866ddd2d7d
Yusuke Gunji <inochideutae@gmail.com>
2020-09-04 17:00:36 +0900

    refactor: consoleエラー対応

--- src/app/article-detail/comment-form/comment-form.component.ts
+++ src/app/article-detail/comment-form/comment-form.component.ts
@@ -3,7 +3,7 @@ import { FormControl, Validators } from '@angular/forms';
 import { CommentService } from 'src/app/services/comment.service';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, tap, take } from 'rxjs/operators';
 import { AuthService } from 'src/app/services/auth.service';
 import { User } from 'src/app/interfaces/users';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -46,7 +46,7 @@ export class CommentFormComponent implements OnInit {
     this.authService.user$
       .pipe(
         map((user) => {
-          return user.uid;
+          return user?.uid;
         })
       )
       .subscribe((uid) => {
