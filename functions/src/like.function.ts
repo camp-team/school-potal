47d7aa4f2b55ae8c48ddfe6b7bc4a65ac26c799d
Yusuke Gunji <inochideutae@gmail.com>
2020-09-07 02:11:24 +0900

    feat(like.function): べき等追加

--- functions/src/like.function.ts
+++ functions/src/like.function.ts
@@ -1,5 +1,6 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
+import { shouldEventRun, markEventTried } from './utils/firebase-util';
 
 const db = admin.firestore();
 
@@ -7,16 +8,32 @@ export const countUpLiked = functions
   .region('asia-northeast1')
   .firestore.document('articles/{articleId}/likedUids/{uid}')
   .onCreate(async (snap, context) => {
-    return db
-      .doc(`articles/${context.params.articleId}`)
-      .update('likeCount', admin.firestore.FieldValue.increment(1));
+    const eventId = context.eventId;
+    return shouldEventRun(eventId).then(async (should) => {
+      if (should) {
+        await db
+          .doc(`articles/${context.params.articleId}`)
+          .update('likeCount', admin.firestore.FieldValue.increment(1));
+        return markEventTried(eventId);
+      } else {
+        return;
+      }
+    });
   });
 
 export const countDownLiked = functions
   .region('asia-northeast1')
   .firestore.document('articles/{articleId}/likedUids/{uid}')
   .onDelete(async (snap, context) => {
-    return db
-      .doc(`articles/${context.params.articleId}`)
-      .update('likeCount', admin.firestore.FieldValue.increment(-1));
+    const eventId = context.eventId;
+    return shouldEventRun(eventId).then(async (should) => {
+      if (should) {
+        await db
+          .doc(`articles/${context.params.articleId}`)
+          .update('likeCount', admin.firestore.FieldValue.increment(-1));
+        return markEventTried(eventId);
+      } else {
+        return;
+      }
+    });
   });
