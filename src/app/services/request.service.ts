0fe3d15351ec1d3d0f9df09cf332971c2429043d
Yusuke Gunji <inochideutae@gmail.com>
2020-10-07 07:25:00 +0900

    feat(request.service): request.service追加

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { firestore } from 'firebase';
-import { Request } from '../interfaces/request';
+import { combineLatest, Observable, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { Request, RequestWithUser } from '../interfaces/request';
+import { User } from '../interfaces/users';
 import { UserService } from './user.service';
 
 @Injectable({
@@ -18,5 +21,50 @@ export class RequestService {
       createdAt: firestore.Timestamp.now(),
     };
     return this.db.doc<Request>(`requests/${id}`).set(value);
+  }
+
+  updateRequest(request: Request): Promise<void> {
+    return this.db.doc<Request>(`requests/${request.id}`).update({
+      ...request,
+    });
+  }
+
+  deleteRequest(request: Request): Promise<void> {
+    return this.db.doc<Request>(`requests/${request.id}`).delete();
+  }
+
+  getRequestsWithUser(): Observable<RequestWithUser[]> {
+    return this.db
+      .collection<Request>(`requests`, (ref) =>
+        ref.orderBy('createdAt', 'desc').limit(30)
+      )
+      .valueChanges()
+      .pipe(
+        switchMap((requests: Request[]) => {
+          if (requests.length) {
+            const unduplicatedUids: string[] = Array.from(
+              new Set(requests.map((request) => request.uid))
+            );
+            const users$: Observable<User[]> = combineLatest(
+              unduplicatedUids.map((uid) => this.userService.getUserData(uid))
+            );
+            return combineLatest([of(requests), users$]);
+          } else {
+            return of([]);
+          }
+        }),
+        map(([requests, users]) => {
+          if (requests?.length) {
+            return requests.map((request: Request) => {
+              return {
+                ...request,
+                user: users.find((user: User) => request.uid === user?.uid),
+              };
+            });
+          } else {
+            return [];
+          }
+        })
+      );
   }
 }
