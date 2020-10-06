d4c5fe2d83f65d68dd8fe1e02e8045e957e9ac6f
Yusuke Gunji <inochideutae@gmail.com>
2020-10-06 23:11:59 +0900

    feat(interface): Request追加

---
+++ src/app/interfaces/request.ts
@@ -0,0 +1,14 @@
+import { firestore } from 'firebase';
+import { User } from '../interfaces/users';
+
+export interface Request {
+  id: string;
+  title: string;
+  body: string;
+  createdAt: firestore.Timestamp;
+  uid: string;
+}
+
+export interface RequestWithUser extends Request {
+  user: User;
+}
