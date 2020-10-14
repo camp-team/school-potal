fc387061b9e678a67c3b399fd46fe5264f9234e4
Yusuke Gunji <inochideutae@gmail.com>
2020-10-14 18:21:20 +0900

    feat(footer): homeにフッター追加

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent implements OnInit {
+  isProcessing: boolean;
+
+  commentForm = new FormControl('', [Validators.required, Validators.email]);
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  submit() {}
+}
