c8276873f674fc4d8ca7343b2f7512af999097b0
Yusuke Gunji <inochideutae@gmail.com>
2020-10-14 18:18:59 +0900

    feat(sendgrid): sendgrid導入、機能追加

--- functions/src/create-user.function.ts
+++ functions/src/create-user.function.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import { sendEmail } from './sendgrid.function';
 
 export const db = admin.firestore();
 
@@ -15,5 +16,34 @@ export const createUser = functions
       createdAt: new Date(),
       isAdmin: false,
       plan: 'free',
+    });
+  });
+
+export const sendEmailCreateUser = functions
+  .region('asia-northeast1')
+  .firestore.document('users/{uid}')
+  .onCreate((snap) => {
+    const user = snap.data();
+    functions.logger.info(user);
+    return sendEmail({
+      to: user.email,
+      templateId: 'd-347a1159a5a44b84a31c3a4694d1c177',
+      dynamicTemplateData: {
+        name: user.name,
+      },
+    });
+  });
+
+export const sendEmailDeleteUser = functions
+  .region('asia-northeast1')
+  .firestore.document('users/{uid}')
+  .onDelete((snap) => {
+    const user = snap.data();
+    return sendEmail({
+      to: user.email,
+      templateId: 'd-7957bcb26523496aa6a56cf954dce54b',
+      dynamicTemplateData: {
+        name: user.name,
+      },
     });
   });
