560aad12ec0a0999ed7d66757674cc6f2c452187
Yusuke Gunji <inochideutae@gmail.com>
2020-10-28 17:32:13 +0900

    feat(all-request): all-requestコンポーネント追加

--- src/app/all-request/all-request/all-request.component.ts
+++ src/app/all-request/all-request/all-request.component.ts
@@ -1,12 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Request, RequestWithUser } from 'src/app/interfaces/request';
+import { RequestComment } from 'src/app/interfaces/request-comment';
+import { User } from 'src/app/interfaces/users';
+import { AuthService } from 'src/app/services/auth.service';
+import { RequestService } from 'src/app/services/request.service';
 
 @Component({
   selector: 'app-all-request',
   templateUrl: './all-request.component.html',
   styleUrls: ['./all-request.component.scss'],
 })
-export class AllRequestComponent implements OnInit {
-  constructor() {}
+export class AllRequestComponent implements OnInit, OnDestroy {
+  subscriptions: Subscription = new Subscription();
+  user: User;
+  isloading: boolean;
+  isComplete: boolean;
+  lastDoc: any;
+  requests: Request[] = [];
 
-  ngOnInit(): void {}
+  requests$: Observable<
+    RequestWithUser[]
+  > = this.requestService.getRequestsWithUser();
+
+  user$: Observable<User> = this.authService.user$;
+
+  constructor(
+    private requestService: RequestService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.subscriptions.add(
+      this.user$.subscribe((user) => {
+        this.user = user;
+      })
+    );
+    this.getRequestsLimited();
+  }
+
+  getRequestsLimited() {
+    this.isloading = true;
+    if (this.isComplete) {
+      this.isloading = false;
+      console.log('complete');
+
+      return;
+    }
+    this.isloading = true;
+    this.requestService.getRequestsWithUser().subscribe((requests) => {
+      if (requests) {
+        if (!requests.length) {
+          this.isComplete = true;
+          this.isloading = false;
+          console.log('no-length');
+
+          return;
+        }
+        this.lastDoc = requests[requests.length - 1];
+        const requestData = requests.forEach((doc) => doc);
+        this.requests.push(...requests);
+        console.log('push');
+
+        this.isloading = false;
+      } else {
+        this.isComplete = true;
+        this.isloading = true;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 }
